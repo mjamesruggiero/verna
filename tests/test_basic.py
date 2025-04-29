9b1ef632ab505d49976b2840847fc9762e6e341b
mjamesruggiero <mjamesruggiero@gmail.com>
2025-04-29 15:48:03 -0700

    Can create paths with canonical names

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,38 @@
 from context import verna
 from verna import core
+import pytest
+import datetime
+from pathlib import Path
 
+
+# TODO fix this brittle test
 def test_count_files_by_extension():
-    test_dir = "/Users/michael/code/mr/verna"
+    test_dir = "/Users/michaelruggiero/code/mr/verna"
     sut = core.count_files_by_extension(test_dir)
     assert sut[".py"] == 4
+
+
+def test_can_generate_datestring():
+    d = datetime.datetime(1967, 9, 30)
+    sut = core.canonical_date(d)
+    assert sut == "1967_09_30"
+
+
+def test_bad_datetime_throws():
+    d = "NOT A DATETIME"
+    with pytest.raises(AttributeError, match=r".*not a valid datetime*"):
+        core.canonical_date(d)
+
+
+def test_can_build_new_filepath():
+    label = "fun with kids"
+    dt = datetime.datetime(1969, 4, 21)
+    base_dir = "/Users/davidlynch"
+    sut = core.mk_filepath(base_dir, label, dt)
+    assert sut == Path("/Users/davidlynch/1969_04_21_fun_with_kids")
+
+
+def test_snake_case():
+    expected = "this_isnt_a_problem"
+    sut = core.snake_case("This Isn't A-Problem")
+    assert sut == expected
--- verna/core.py
+++ verna/core.py
@@ -1,6 +1,10 @@
 import os
 import argparse
 from collections import Counter
+import datetime
+from re import sub
+from pathlib import Path, PosixPath
+
 
 def count_files_by_extension(directory):
     file_counts = Counter()
@@ -12,8 +16,11 @@ def count_files_by_extension(directory):
 
     return file_counts
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Count files by extension in a directory.")
+    parser = argparse.ArgumentParser(
+        description="Count files by extension in a directory."
+    )
     parser.add_argument("directory", type=str, help="Path to the directory to scan")
     args = parser.parse_args()
 
@@ -25,6 +32,33 @@ def main():
 
     for ext, count in sorted(file_counts.items()):
         print(f"{ext if ext else '[no extension]'}\t{count}")
+
+
+def canonical_date(dt: datetime) -> str:
+    try:
+        return dt.strftime("%Y_%m_%d")
+    except AttributeError as e:
+        raise AttributeError(f"arg '{dt}' not a valid datetime")
+
+
+def snake_case(s: str) -> str:
+    # Replace hyphens with spaces, then apply regular expression substitutions for title case conversion
+    # and add an underscore between words, finally convert the result to lowercase
+    return "_".join(
+        sub(
+            "([A-Z][a-z]+)",
+            r" \1",
+            sub("([A-Z]+)", r" \1", s.replace("-", " ").replace("'", "")),
+        ).split()
+    ).lower()
+
+
+def mk_filepath(base_dir, label, dt=datetime.datetime.now()) -> PosixPath:
+    formatted_date = canonical_date(dt)
+    formatted_label = snake_case(label)
+    p = Path(base_dir)
+    return p / f"{formatted_date}_{formatted_label}"
+
 
 if __name__ == "__main__":
     main()
